67e9244cc457efe70074148c4dae0dfd72a9c771
resetes12 <diploporigon@gmail.com>
2025-11-30 18:00:15 +0100

    Fixed "Evo level time male" text

--- src/pokedex_plus_hgss.c
+++ src/pokedex_plus_hgss.c
@@ -6954,7 +6954,7 @@ static u8 PrintEvolutionTargetSpeciesAndMethod(u8 taskId, u16 species, u8 depth,
             break;
         case EVO_LEVEL_MALE_MORNING:
             ConvertIntToDecimalStringN(gStringVar2, gEvolutionTable[species][i].param, STR_CONV_MODE_LEADING_ZEROS, EVO_SCREEN_LVL_DIGITS); //level
-            StringExpandPlaceholders(gStringVar4, gText_EVO_LEVEL_TIME_FEMALE );
+            StringExpandPlaceholders(gStringVar4, gText_EVO_LEVEL_TIME_MALE );
             break;
         case EVO_ITEM_HOLD:
             item = gEvolutionTable[species][i].param; //item
